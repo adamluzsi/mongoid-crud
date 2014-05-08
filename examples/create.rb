a3667f579964ac4ef968a3d9d0edb171e97ff4f0
Adam Luzsi <adam.luzsi@ppt-consulting.net>
2014-05-08 11:16:47 +0200

    update create example

--- examples/create.rb
+++ examples/create.rb
@@ -9,7 +9,6 @@ test_c= TestC.__create__( hello: "embeds one", parent_id: test_a['_id'] )
 test_d= TestD.__create__( hello: "embeds many in embeded one", parent_id: test_c['_id'] )
 
 #> puts into json
-puts TestA._read( _id: test_a['_id'] )
 
 # [{"_id":"536b298b241548e55a000001","created_at":"2014-05-08T08:51:55+02:00","hello":"world","test_b":[{"_id":"536b298b241548e55a000002","created_at":"2014-05-08T08:51:55+02:00","hello":"world","updated_at":"2014-05-08T08:51:55+02:00","world":"no"}],"test_c":{"_id":"536b298b241548e55a000003","created_at":"2014-05-08T08:51:55+02:00","hello":"embeds one","updated_at":"2014-05-08T08:51:55+02:00"},"updated_at":"2014-05-08T08:51:55+02:00"}]
 
